c76906ceba744e016949364331ad1813231fa1cb
Sanket Berde <sanket.berde@gmail.com>
2015-08-10 00:28:11 +0530

    Iterative processing
    The Script is modified to keep on processing images recursively.

--- deepdream/process_images_once.sh
+++ deepdream/process_images_once.sh
@@ -3,6 +3,7 @@
 # We will be running this script inside the visionai/clouddream Docker image
 # Copyright vision.ai, 2015
 
+
 cd /opt/deepdream/inputs
 find . -type f -not -path '*/\.*' -print0 | while read -d $'\0' f;
 do
@@ -10,15 +11,39 @@ do
     if [ -e outputs/${f} ];
     then
 	echo "File ${f} already processed"
-    else
-	echo "Deepdream" ${f}
-	chmod gou+r inputs/${f}
+	echo "Processing again"
+
+	chmod 777 inputs/${f}
+
+	rm inputs/${f}
+	cp outputs/${f} inputs/${f}
+
 	cp inputs/${f} input.jpg
 	python deepdream.py
 	ERROR_CODE=$?
 	echo "Error Code is" ${ERROR_CODE}
+
+	rm input.jpg
 	cp output.jpg outputs/${f}
+	cp output.jpg inputs/${f}
 	rm output.jpg
+
+	echo "Just created" outputs/${f}
+
+    else
+	echo "Deepdream" ${f}
+	chmod 777 inputs/${f}
+
+	cp inputs/${f} input.jpg
+	python deepdream.py
+	ERROR_CODE=$?
+	echo "Error Code is" ${ERROR_CODE}
+
+	rm input.jpg
+	cp output.jpg outputs/${f}
+	cp output.jpg inputs/${f}
+	rm output.jpg
+
 	echo "Just created" outputs/${f}
     fi
 done
